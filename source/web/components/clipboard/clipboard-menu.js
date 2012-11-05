2a84ebf3885bc97b22a3cfeada21a55c185ccd29
Florian Maul <f.maul@fme.de>
2012-11-05 15:56:46 +0100

    Cleanup I18N

--- source/web/components/clipboard/clipboard-menu.js
+++ source/web/components/clipboard/clipboard-menu.js
@@ -31,35 +31,12 @@
                        { text : this.msg("clipboard.action.copy.here"), classname: "clipboard-copy", onclick : { fn: this.onCopyHere, scope: this } },
                        { text : this.msg("clipboard.action.move.here"), classname: "clipboard-move", onclick : { fn: this.onMoveHere, scope: this } },
                        { text : this.msg("clipboard.action.clear"), classname: "clipboard-clear", onclick : { fn: this.onClearClipboard, scope: this } }
-/*
-                       { text : "Refresh Repository Webscripts", classname: "refresh-menuitem", onclick : { fn: this.onRefreshRepoWebscripts, scope: this } },
-          	      { text : "Refresh Share Webscripts", classname: "refresh-menuitem", onclick : { fn: this.onRefreshShareWebscripts, scope: this } },
-          	      { text : "Refresh Share All (WS, Templates, Config)", classname: "refresh-menuitem", onclick : { fn: this.onRefreshAllShare, scope: this } },
-          	      { text : "Toggle Surfbug", classname: "surfbug-menuitem", onclick : { fn: this.onSurfBug, scope: this } },
-          	      { text : "Toggle Repository Debugger", classname: "debugger-menuitem", onclick : { fn: this.onRepoDebugger, scope: this } },
-          	      { text : "Toggle Share Debugger", classname: "debugger-menuitem", onclick : { fn: this.onShareDebugger, scope: this } },
-          	      { text : "Share Module Deployment", url : Alfresco.constants.URL_PAGECONTEXT + "modules/deploy", target:"_blank" },
-          	      { text : "Javascript Console", classname: "jsconsole-menuitem", url : Alfresco.constants.URL_PAGECONTEXT + "console/admin-console/javascript-console" },
-          	      { text : "Node Browser", classname: "jsconsole-menuitem", url : Alfresco.constants.URL_PAGECONTEXT + "console/admin-console/node-browser" },
-          	      
-          	      // TODO: get correct URL, maybe open in iframe
-          	      { text : "Workflow Console", url : this.options.explorerBaseUrl + "/faces/jsp/admin/workflow-console.jsp", target:"_blank" },
-
-          	      // TODO: check if SOLR is active, get correct URL, maybe open in iframe        	      
-          	      { text : "Solr Admin Console", url : this.options.solrAdminUrl, target:"_blank" },
-          	      
-          	      // TODO: check if lucene is active, get correct URL, maybe open in iframe        	      
-          	      { text : "Lucene Index Check", url : this.options.explorerBaseUrl + "/service/enterprise/admin/indexcheck", target:"_blank" }
-          	       
-          	      // TODO: Open Explorer (in IFrame)
-          	      // TODO: Toggle Minification. How to change the config dynamically?
-*/
           	 ]);
           
           this.widgets.clipboardButton = new YAHOO.widget.Button(
                    { id: container.id+"-clipboard",
                      type: "menu",
-                      label: "Clipboard",
+                      label: this.msg("button.label.clipboard"),
                       menu: menu,
                       lazyloadmenu: true, 
                       container: container.id
@@ -80,10 +57,10 @@
     	  this.widgets.clipboardButton.set('disabled', clipsize == 0);
     	  
     	 if (clipsize == 0) {
-    		 this.widgets.clipboardButton.set("label", "Clipboard");
+    		 this.widgets.clipboardButton.set("label", this.msg("button.label.clipboard"));
     	 }
     	 else {
-    		 this.widgets.clipboardButton.set("label", "Clipboard (" + clipsize + ")");
+    		 this.widgets.clipboardButton.set("label", this.msg("button.label.clipboard") + " (" + clipsize + ")");
     	 }
       },
       
