818ebd874bcb2b38514f412356a09477e7d0aeda
Florian Maul <f.maul@fme.de>
2016-11-02 15:51:22 +0100

    Adds send as email actions to clipboard

--- alfresco-clipboard-share/src/main/amp/web/components/clipboard/clipboard-menu.js
+++ alfresco-clipboard-share/src/main/amp/web/components/clipboard/clipboard-menu.js
@@ -32,7 +32,8 @@
                        { text : this.msg("clipboard.action.link.here"), classname: "clipboard-link", onclick : { fn: this.onLinkHere, scope: this } },
                        { text : this.msg("clipboard.action.move.here"), classname: "clipboard-move", onclick : { fn: this.onMoveHere, scope: this } },
                        { text : this.msg("clipboard.action.download"), classname: "clipboard-download", onclick : { fn: this.onDownloadClipboard, scope: this } },
-                       { text : this.msg("clipboard.action.clear"), classname: "clipboard-clear", onclick : { fn: this.onClearClipboard, scope: this } }
+                       { text : this.msg("clipboard.action.clear"), classname: "clipboard-clear", onclick : { fn: this.onClearClipboard, scope: this } },
+                       { text : this.msg("clipboard.action.send.attachment"), classname: "clipboard-send-attachment", onclick : { fn: this.onSendAttachment, scope: this } }
           	 ]);
           
           this.widgets.clipboardButton = new YAHOO.widget.Button(
@@ -94,6 +95,86 @@
       onClearClipboard : function ClipboardMenu_onClearClipboard() {
     	  var clip = new Alfresco.service.Clipboard();
     	  clip.removeAll();
+      },
+      
+      onSendAttachment : function ClipboardMenu_onSendAttachment() {
+    	  var clipboardService = new Alfresco.service.Clipboard();
+    	  var nodeRefs = [];
+ 	  	 var nodes = clipboardService.getAll();
+ 		for (var i = 0; i < nodes.length; i++){
+			nodeRefs.push({"nodeRef": nodes[i].nodeRef});
+		}
+ 			// Failure callback function
+ 		    var fnFailure = function ASCCOM__onOK_failure(p_data)
+ 		    {
+ 		    	if(p_data.json.message){
+ 		       Alfresco.util.PopupManager.displayMessage(
+ 		       {
+ 		          text: this.msg("message.failure" +"." + p_data.json.message)
+ 		       });
+ 		    	} else {
+ 	 		       Alfresco.util.PopupManager.displayMessage(
+ 	 		 		       {
+ 	 		 		          text: this.msg("message.failure")
+ 	 		 		       });		
+ 		    	}
+ 		    };
+ 		    
+ 		    // Success callback function
+ 		    var fnSuccess = function ASCCOM__onOK_success(p_data)
+ 		    {
+ 		       Alfresco.util.PopupManager.displayMessage(
+ 		       {
+ 		          text: this.msg("message.success")
+ 		       });
+ 		       
+ 		       YAHOO.Bubbling.fire("metadataRefresh");
+ 		    };
+ 			
+ 			var doclist = Alfresco.util.ComponentManager.find({name : "Alfresco.DocumentList"})[0];
+ 			
+ 			var folderRef = new Alfresco.util.NodeRef(doclist.doclistMetadata.parent.nodeRef);
+ 			
+ 			// Construct the data object for the genericAction call
+ 		    doclist.modules.actions.genericAction(
+ 		    {
+ 		       success:
+ 		       {
+ 		          callback:
+ 		          {
+ 		             fn: fnSuccess,
+ 		             scope: this
+ 		          }
+ 		       },
+ 		       failure:
+ 		       {
+ 		          callback:
+ 		          {
+ 		             fn: fnFailure,
+ 		             scope: this
+ 		          }
+ 		       },
+ 		       webscript:
+ 		       {
+ 		          method: Alfresco.util.Ajax.POST,
+ 		          name: "sendMail",
+ 		          stem: Alfresco.constants.PROXY_URI
+ 		       },
+ 		       wait:
+ 		       {
+ 		          message: this.msg("message.please-wait")
+ 		       },
+ 		       config:
+ 		       {
+ 		          requestContentType: Alfresco.util.Ajax.JSON,
+ 		          dataObj:
+ 		          {
+ 		             nodeRefs: nodeRefs,
+ 		             subject: this.msg("message.mail.subject")
+ 		          }
+ 		       }
+ 		    });
+ 	     
       },
       
       _copyOrMove : function ClipboardMenu__copyOrMove(action) {
@@ -175,7 +256,8 @@
 	          requestContentType: Alfresco.util.Ajax.JSON,
 	          dataObj:
 	          {
-	             nodeRefs: nodeRefs
+	             nodeRefs: nodeRefs,
+	             subject: this.msg("message.subject")
 	          }
 	       }
 	    });
